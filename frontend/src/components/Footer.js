f3a16f4ee52551a80155d224e1e00365b0ca76fa
Angel Palomar <angelpg.123@hotmail.com>
2020-10-28 01:51:21 -0600

    Página web principal terminada.

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,107 @@
 import React from 'react'
+import { Box, Grid, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
+
+/**Imagenes */
+import math_logo from '../assets/images/logos/MathParadiseLogo.svg'
+import uteq_logo from '../assets/images/logos/uteq_logo.svg'
+
+const useStyles = makeStyles((theme) => ({
+
+    root: {
+        padding: theme.spacing(2),
+        marginTop: theme.spacing(3),
+        width: '100%',
+        background: '#FFF',
+        color: '#2A55FF'
+    },
+    logos: {
+        [theme.breakpoints.up('xs')]: {
+            width: '25%'
+        },
+        [theme.breakpoints.up('sm')]: {
+            width: '15%'
+        },
+        [theme.breakpoints.up('md')]: {
+            width: '15%'
+        },
+        [theme.breakpoints.up('lg')]: {
+            width: '10%'
+        },
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+    },
+    logosContainer: {
+        display: 'flex',
+        alignContent: 'center',
+        justifyContent: 'center',
+        marginBottom: theme.spacing(1)
+    },
+    rights: {
+        display: 'flex',
+        alignContent: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginTop: theme.spacing(3),
+    },
+    title: {
+        marginBottom: theme.spacing(1)
+    },
+    contactContainer: {
+        textAlign: 'right'
+    },
+    link: {
+        textDecoration: 'none',
+        color: '#00487C',
+        '&:hover': {
+            color: '#00487C'
+        }
+    }
+}))
 
 function Footer() {
+    const classes = useStyles()
     return (
-        <div>
+        <Box className={classes.root} boxShadow={4}>
+            <Box className={classes.logosContainer}>
+                <img src={math_logo} alt="math_logo.svg" className={classes.logos} />
+                <img src={uteq_logo} alt="uteq_logo.svg" className={classes.logos} />
+            </Box>
+            <Grid container>
+                <Grid item lg={2} md={2} sm={6} xs={6}>
+                    <Typography variant="h6" className={classes.title}>Social</Typography>
+                    <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className={classes.link}>Facebook</a>
+                    <br />
+                    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" className={classes.link}>Instagram</a>
+                </Grid>
+                <Grid item lg={3} md={3} sm={6} xs={6}>
+                    <Typography variant="h6" className={classes.title}>Más información</Typography>
+                    <a href="http://www.uteq.edu.mx/" target="_blank" rel="noopener noreferrer" className={classes.link}>
+                        Universidad Tecnológica de Querétaro
+                    </a>
+                    <br />
+                    <a href="/" className={classes.link}>
+                        Créditos
+                    </a>
+                </Grid>
+                <Grid item lg={7} md={7} sm={12} xs={12} className={classes.contactContainer}>
+                    <Typography variant="h6" className={classes.title}>Contacto</Typography>
+                    <a href="mailto:cruzangelp@gmail.com" rel="noopener noreferrer" className={classes.link}>
+                        Equipo de desarrollo
+                    </a>
+                    <br />
+                    <a href="mailto:mathparadise@gmail.com" rel="noopener noreferrer" className={classes.link}>
+                        Feedback
+                    </a>
+                </Grid>
+            </Grid>
 
-        </div>
+            <Box className={classes.rights}>
+                <Typography>Math Paradise&reg; - Todos los derechos reservados - 2020</Typography>
+                <Typography>Versión 1.0.0</Typography>
+            </Box>
+        </Box>
     )
 }
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,73 @@
 import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Box, Grid, Typography, Button } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
 
+/**Componentes */
 import PublicHeader from '../components/PublicHeader'
+import Footer from '../components/Footer'
+
+/**Imágenes */
+import banner1 from '../assets/images/banners/banner1.svg'
+import banner2 from '../assets/images/banners/banner2.svg'
+import banner3 from '../assets/images/banners/banner3.svg'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        padding: theme.spacing(5),
+    },
+    bannerContainer: {
+        display: 'flex',
+        alignContent: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        width: '50vh',
+        background: "linear-gradient(45deg, #2A55FF, #15FFD4)",
+        padding: theme.spacing(1),
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1),
+        color: '#FFF',
+        fontSize: '22px'
+    },
+    forms: {
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        marginTop: theme.spacing(10),
+    },
+    titles: {
+        color: '#2A55FF',
+        textAlign: 'center'
+    },
+    banners: {
+        [theme.breakpoints.up('xs')]: {
+            width: '95%'
+        },
+        [theme.breakpoints.up('sm')]: {
+            width: '90%'
+        },
+        [theme.breakpoints.up('md')]: {
+            width: '85%'
+        },
+        [theme.breakpoints.up('lg')]: {
+            width: '100vh'
+        },
+    },
+    link: {
+        textDecoration: 'none',
+        color: theme.palette.text.primary,
+        '&:hover': {
+            textDecoration: 'none',
+            color: theme.palette.text.primary,
+        }
+    },
+}))
 
 function Home() {
+    const classes = useStyles()
 
     useEffect(() => {
         document.title = 'Math Paradise'
@@ -11,6 +76,43 @@ function Home() {
     return (
         <>
             <PublicHeader />
+            <Grid container>
+                <Grid item lg={6} md={6} sm={12} xs={12}>
+                    <Box className={classes.root}>
+                        <Box className={classes.bannerContainer}>
+                            <img src={banner1} alt="banner1" className={classes.banners} />
+                        </Box>
+                    </Box>
+                </Grid>
+                <Grid item lg={6} md={6} sm={12} xs={12}>
+                    <Box className={classes.root}>
+                        <Typography variant="h4" className={classes.titles}>¡Empieza a jugar!</Typography>
+
+                        <Box className={classes.forms}>
+                            <Link to="/login" className={classes.link}>
+                                <Button className={classes.button} variant="contained">Iniciar sesión</Button>
+                            </Link>
+                            <Link to="/sign-up" className={classes.link}>
+                                <Button className={classes.button} variant="contained">Crear una cuenta</Button>
+                            </Link>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+
+            <Box className={classes.root}>
+                <Box className={classes.bannerContainer}>
+                    <img src={banner2} alt="banner2" className={classes.banners} />
+                </Box>
+            </Box>
+
+            <Box className={classes.root}>
+                <Box className={classes.bannerContainer}>
+                    <img src={banner3} alt="banner3" className={classes.banners} />
+                </Box>
+            </Box>
+
+            <Footer />
         </>
     )
 }
